5afb96aa0f6265b38d996d91f091043a07714f64
Chris Lord <chris.lord@instaclustr.com>
2018-02-02 23:13:06 +1100

    Move harvesters into their own class

--- src/harvester.py
+++ src/harvester.py
@@ -10,71 +10,100 @@ __pragma__('noalias', 'type')
 __pragma__('noalias', 'update')
 
 
-def run_harvester(creep):
-    """
-    Runs a creep as a generic harvester.
-    :param creep: The creep to run
-    """
+class Harvester(object):
 
-    # If we're full, stop filling up and remove the saved source
-    if creep.memory.filling and _.sum(creep.carry) >= creep.carryCapacity:
-        creep.memory.filling = False
-        del creep.memory.source
-    # If we're empty, start filling again and remove the saved target
-    elif not creep.memory.filling and creep.carry.energy <= 0:
-        creep.memory.filling = True
-        del creep.memory.target
+    BODY_TYPE = {
+        'small': {
+            'components': [WORK, CARRY, MOVE, MOVE],
+            'price': 250
+        },
+        'medium': {
+            'components': [WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE],
+            'price': 450
+        },
+        'large': {
+            'components': [WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY,
+                           MOVE, MOVE, MOVE, MOVE, MOVE, MOVE],
+            'price': 900
+        }
+    }
 
-    if creep.memory.filling:
-        # If we have a saved source, use it
-        if creep.memory.source:
-            source = Game.getObjectById(creep.memory.source)
-        else:
-            # Get a random new source and save it
-            source = _.sample(creep.room.find(FIND_SOURCES))
-            creep.memory.source = source.id
+    @staticmethod
+    def create_harvester(body, spawn):
+        body_type = Harvester.BODY_TYPE[body]
+        if (body not in BODY_TYPE) and (body_type['price'] >= spawn.room.energyAvailable):
+            console.log(f'Unable to create harvester: {body_type}')
+        elif not spawn.spawning:
+            spawn.spawnCreep(BODY_TYPE[body]['components'], None)
+            console.log('should be spawning a new creep')
 
-        # If we're near the source, harvest it - otherwise, move to it.
-        if creep.pos.isNearTo(source):
-            result = creep.harvest(source)
-            if result != OK:
-                print("[{}] Unknown result from creep.harvest({}): {}".format(creep.name, source, result))
-        else:
-            creep.moveTo(source)
-    else:
-        # If we have a saved target, use it
-        if creep.memory.target:
-            target = Game.getObjectById(creep.memory.target)
-        else:
-            # Get a random new target.
-            target = _(creep.room.find(FIND_STRUCTURES)) \
-                .filter(lambda s: ((s.structureType == STRUCTURE_SPAWN or s.structureType == STRUCTURE_EXTENSION)
-                                   and s.energy < s.energyCapacity) or s.structureType == STRUCTURE_CONTROLLER) \
-                .sample()
-            creep.memory.target = target.id
 
-        # If we are targeting a spawn or extension, we need to be directly next to it - otherwise, we can be 3 away.
-        if target.energyCapacity:
-            is_close = creep.pos.isNearTo(target)
-        else:
-            is_close = creep.pos.inRangeTo(target, 3)
+    @staticmethod
+    def run_harvester(creep):
+        """
+        Runs a creep as a generic harvester.
+        :param creep: The creep to run
+        """
 
-        if is_close:
-            # If we are targeting a spawn or extension, transfer energy. Otherwise, use upgradeController on it.
-            if target.energyCapacity:
-                result = creep.transfer(target, RESOURCE_ENERGY)
-                if result == OK or result == ERR_FULL:
-                    del creep.memory.target
-                else:
-                    print("[{}] Unknown result from creep.transfer({}, {}): {}".format(
-                        creep.name, target, RESOURCE_ENERGY, result))
+        # If we're full, stop filling up and remove the saved source
+        if creep.memory.filling and _.sum(creep.carry) >= creep.carryCapacity:
+            creep.memory.filling = False
+            del creep.memory.source
+        # If we're empty, start filling again and remove the saved target
+        elif not creep.memory.filling and creep.carry.energy <= 0:
+            creep.memory.filling = True
+            del creep.memory.target
+
+        if creep.memory.filling:
+            # If we have a saved source, use it
+            if creep.memory.source:
+                source = Game.getObjectById(creep.memory.source)
             else:
-                result = creep.upgradeController(target)
+                # Get a random new source and save it
+                source = _.sample(creep.room.find(FIND_SOURCES))
+                creep.memory.source = source.id
+
+            # If we're near the source, harvest it - otherwise, move to it.
+            if creep.pos.isNearTo(source):
+                result = creep.harvest(source)
                 if result != OK:
-                    print("[{}] Unknown result from creep.upgradeController({}): {}".format(
-                        creep.name, target, result))
-                # Let the creeps get a little bit closer than required to the controller, to make room for other creeps.
-                if not creep.pos.inRangeTo(target, 2):
-                    creep.moveTo(target)
+                    print("[{}] Unknown result from creep.harvest({}): {}".format(creep.name, source, result))
+            else:
+                creep.moveTo(source)
         else:
-            creep.moveTo(target)
+            # If we have a saved target, use it
+            if creep.memory.target:
+                target = Game.getObjectById(creep.memory.target)
+            else:
+                # Get a random new target.
+                target = _(creep.room.find(FIND_STRUCTURES)) \
+                    .filter(lambda s: ((s.structureType == STRUCTURE_SPAWN or s.structureType == STRUCTURE_EXTENSION)
+                                       and s.energy < s.energyCapacity) or s.structureType == STRUCTURE_CONTROLLER) \
+                    .sample()
+                creep.memory.target = target.id
+
+            # If we are targeting a spawn or extension, we need to be directly next to it - otherwise, we can be 3 away.
+            if target.energyCapacity:
+                is_close = creep.pos.isNearTo(target)
+            else:
+                is_close = creep.pos.inRangeTo(target, 3)
+
+            if is_close:
+                # If we are targeting a spawn or extension, transfer energy. Otherwise, use upgradeController on it.
+                if target.energyCapacity:
+                    result = creep.transfer(target, RESOURCE_ENERGY)
+                    if result == OK or result == ERR_FULL:
+                        del creep.memory.target
+                    else:
+                        print("[{}] Unknown result from creep.transfer({}, {}): {}".format(
+                            creep.name, target, RESOURCE_ENERGY, result))
+                else:
+                    result = creep.upgradeController(target)
+                    if result != OK:
+                        print("[{}] Unknown result from creep.upgradeController({}): {}".format(
+                            creep.name, target, result))
+                    # Let the creeps get a little bit closer than required to the controller, to make room for other creeps.
+                    if not creep.pos.inRangeTo(target, 2):
+                        creep.moveTo(target)
+            else:
+                creep.moveTo(target)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import harvester
+from harvester import Harvester
 # defs is a package which claims to export all constants and some JavaScript objects, but in reality does
 #  nothing. This is useful mainly when using an editor like PyCharm, so that it 'knows' that things like Object, Creep,
 #  Game, etc. do exist.
@@ -25,7 +25,7 @@ def main():
     # Run each creep
     for name in Object.keys(Game.creeps):
         creep = Game.creeps[name]
-        harvester.run_harvester(creep)
+        Harvester.run_harvester(creep)
 
     # Run each spawn
     for name in Object.keys(Game.spawns):
@@ -35,15 +35,15 @@ def main():
             num_creeps = _.sum(Game.creeps, lambda c: c.pos.roomName == spawn.pos.roomName)
             # If there are no creeps, spawn a creep once energy is at 250 or more
             if num_creeps < 0 and spawn.room.energyAvailable >= 250:
-                spawn.createCreep([WORK, CARRY, MOVE, MOVE])
+                Harvester.create_harvester('small', spawn)
             # If there are less than 15 creeps but at least one, wait until all spawns and extensions are full before
             # spawning.
             elif num_creeps < 15 and spawn.room.energyAvailable >= spawn.room.energyCapacityAvailable:
                 # If we have more energy, spawn a bigger creep.
                 if spawn.room.energyCapacityAvailable >= 350:
-                    spawn.createCreep([WORK, CARRY, CARRY, MOVE, MOVE, MOVE])
+                    Harvester.create_harvester('medium', spawn)
                 else:
-                    spawn.createCreep([WORK, CARRY, MOVE, MOVE])
+                    Harvester.create_harvester('small', spawn)
 
 
 module.exports.loop = main
