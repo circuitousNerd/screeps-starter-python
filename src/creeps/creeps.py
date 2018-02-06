62556e849da10868dbf56f2f7fac3a685e96e9ef
Chris Lord <chris.lord@dtspur.com>
2018-02-06 20:08:26 +1100

    Genericise the creation of creeps

--- src/creeps/creeps.py
+++ src/creeps/creeps.py
@@ -18,9 +18,48 @@ __pragma__('noalias', 'update')
 
 class Creeps(object):
 
+    BODY_TYPE = {
+        'small': {
+            'components': [WORK, CARRY, MOVE, MOVE],
+            'price': 250
+        },
+        'medium': {
+            'components': [WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE],
+            'price': 450
+        },
+        'large': {
+            'components': [WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY,
+                           MOVE, MOVE, MOVE, MOVE, MOVE, MOVE],
+            'price': 900
+        }
+    }
+
     @staticmethod
-    def create_creep(creep):
-        pass
+    def create_creep(body, spawn, role):
+        console.log(JSON.stringify(role))
+
+        role = role
+        creep_name = role + spawn.name + Game.time
+
+        if body in Creeps.BODY_TYPE:
+            body_type = Creeps.BODY_TYPE[body]
+        else:
+            console.log('Invalid body type')
+
+        if not role:
+            exit()
+        elif body_type['price'] >= spawn.room.energyAvailable:
+            console.log(f'Unable to create creep: {body_type}')
+        elif (not spawn.spawning) and (spawn.memory.last_spawn is not role):
+            spawn.memory.last_spawn = role
+            spawn_return = spawn.spawnCreep(body_type['components'], creep_name, {
+                'memory': {
+                    'role': role
+                }
+            })
+            if spawn_return is OK:
+                spawn.memory["last_spawn"] = role
+                console.log(f'should be spawning a new creep named: {creep_name} of size {body_type}')
 
     @staticmethod
     def run_creep(creep):
